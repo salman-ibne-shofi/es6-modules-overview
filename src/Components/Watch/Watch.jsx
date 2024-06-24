421bee3c056385b130137ba207c90c45a976c519
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-24 14:07:41 +0600

    ES6 Modules, Import Default, Relative Path

---
+++ src/Components/Watch/Watch.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Watch.css'
+import Watch from '../Watch/Watch'
+
+const Watch = () => {
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Watch;
